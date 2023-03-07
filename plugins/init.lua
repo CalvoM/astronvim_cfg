4546de1852bb98b00c165d9c93f4cb808ae7bae1
CalvoM <mwadimemakokha@gmail.com>
2023-03-07 16:27:37 +0300

    lazy intro

--- plugins/init.lua
+++ plugins/init.lua
@@ -21,24 +21,26 @@ return {
         require("colorbuddy").colorscheme "cobalt2"
       end,
     },
-    { 'preservim/tagbar' },
+    { 'preservim/tagbar', lazy=false,},
     { 'projekt0n/github-nvim-theme', tag = 'v0.0.7' },
     { "AlexvZyl/nordic.nvim" },
-    { "rcarriga/nvim-dap-ui",        requires = { "mfussenegger/nvim-dap" } },
-    { 'Wansmer/treesj',              requires = { 'nvim-treesitter' } },
-    { "wfxr/minimap.vim" },
-    { "HiPhish/jinja.vim" },
+    { "rcarriga/nvim-dap-ui",        requires = { "mfussenegger/nvim-dap" }, lazy=false,},
+    { 'Wansmer/treesj',              requires = { 'nvim-treesitter' }, lazy=false },
+    { "wfxr/minimap.vim", lazy=false, },
+    { "HiPhish/jinja.vim", lazy=false,},
     {
         's1n7ax/nvim-search-and-replace',
+        lazy = false,
         config = function() require 'nvim-search-and-replace'.setup() end,
     },
-    { "xiyaowong/nvim-transparent" },
+    { "xiyaowong/nvim-transparent", lazy=false, },
     { 'f-person/git-blame.nvim', lazy=false, },
     { "EdenEast/nightfox.nvim" },
-    { "mfussenegger/nvim-dap" },
+    { "mfussenegger/nvim-dap", lazy=false},
     { "folke/tokyonight.nvim" },
     ["ray-x/lsp_signature.nvim"] = {
         event = "BufRead",
+        lazy = false,
         opt = false,
         config = function()
             require("lsp_signature").setup {
@@ -50,6 +52,7 @@ return {
     ["wakatime/vim-wakatime"] = { event = "BufEnter" },
     { "folke/todo-comments.nvim",
         requires = "nvim-lua/plenary.nvim",
+        lazy = false,
         config = function()
             require("todo-comments").setup {
                 -- your configuration comes here
