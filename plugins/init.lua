3ac047032323cae4e202868be400ee7a3089df79
CalvoM <mwadimemakokha@gmail.com>
2023-03-27 13:39:49 +0300

    lint

--- plugins/init.lua
+++ plugins/init.lua
@@ -10,24 +10,25 @@ return {
             }
         end
     },
-    {'tjdevries/colorbuddy.nvim', 
+    {
+        'tjdevries/colorbuddy.nvim',
         config = false,
         lazy = false,
     },
-    { 'preservim/tagbar', lazy=false, event="BufEnter"},
-    { "rcarriga/nvim-dap-ui",        dependencies = { "mfussenegger/nvim-dap" }, lazy=false,},
-    { 'Wansmer/treesj',              dependencies = { 'nvim-treesitter' }, lazy=false },
-    { "wfxr/minimap.vim", lazy=false, },
-    { "HiPhish/jinja.vim", lazy=false,},
+    { 'preservim/tagbar',     lazy = false,                               event = "BufEnter" },
+    { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" }, lazy = false, },
+    { 'Wansmer/treesj',       dependencies = { 'nvim-treesitter' },       lazy = false },
+    { "wfxr/minimap.vim",     lazy = false, },
+    { "HiPhish/jinja.vim",    lazy = false, },
     {
         's1n7ax/nvim-search-and-replace',
         lazy = false,
         config = function() require 'nvim-search-and-replace'.setup() end,
     },
-    { 'kartikp10/noctis.nvim', dependencies = { 'rktjmp/lush.nvim' }, lazy = false },
-    { "xiyaowong/nvim-transparent", lazy=false, },
-    { 'f-person/git-blame.nvim', lazy=false, },
-    { "mfussenegger/nvim-dap", lazy=false},
+    { 'kartikp10/noctis.nvim',      dependencies = { 'rktjmp/lush.nvim' }, lazy = false },
+    { "xiyaowong/nvim-transparent", lazy = false, },
+    { 'f-person/git-blame.nvim',    lazy = false, },
+    { "mfussenegger/nvim-dap",      lazy = false },
     ["ray-x/lsp_signature.nvim"] = {
         event = "BufRead",
         lazy = false,
@@ -40,7 +41,9 @@ return {
         end,
     },
     ["wakatime/vim-wakatime"] = { event = "BufEnter" },
-    { "folke/todo-comments.nvim",
+    { "IndianBoy42/tree-sitter-just", lazy = false },
+    {
+        "folke/todo-comments.nvim",
         requires = "nvim-lua/plenary.nvim",
         lazy = false,
         config = function()
