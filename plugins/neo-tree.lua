ac18a0d67f8d63f73a51c80aa035e215a43fdb11
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2023-07-05 11:42:20 +0300

    .

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,19 +1,19 @@
 return {
         "nvim-neo-tree/neo-tree.nvim",
         opt = {
-        window = {
-                mappings = {
-                        ["P"] = { "toggle_preview", config = { use_float = true } },
-                        ["o"] = "open",
+                window = {
+                        mappings = {
+                                ["P"] = { "toggle_preview", config = { use_float = true } },
+                                ["o"] = "open",
+                        },
                 },
-        },
-        filesystem = {
-                filtered_items = {
-                        hide_dotfiles = false,
-                        hide_gitignored = false,
-                        hide_hidden = false
+                filesystem = {
+                        filtered_items = {
+                                hide_dotfiles = false,
+                                hide_gitignored = false,
+                                hide_hidden = false
+                        },
                 },
-        },
 
         }
 }
